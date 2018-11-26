9cc6ed4bb5a457f643886f81d476137e8fd717b3
Meredith Rosenthal <mrosenthal@ucvts.org>
2018-11-26 13:01:52 -0500

    hurricane complete

--- js/problem-set-5.js
+++ js/problem-set-5.js
@@ -296,6 +296,31 @@ function hurricane() {
   let windspeed; // DO NOT MODIFY
   ///////////////// DO NOT MODIFY
 
+  do {
+    windspeed = Number(prompt("What is the windspeed of the hurricane?"));
+  } while (windspeed < 0);
+
+  let cat = "";
+
+  if (windspeed >= 157){
+    cat = "Category 5 Hurricane.";
+  } else if (windspeed < 157 && windspeed >= 130) {
+    cat = "Category 4 Hurricane.";
+  } else if (windspeed < 130 && windspeed >= 111) {
+    cat = "Category 3 Hurricane.";
+  } else if (windspeed < 111 && windspeed >= 96) {
+    cat = "Category 2 Hurricane.";
+  } else if (windspeed < 96 && windspeed >= 74) {
+    cat = "Category 1 Hurricane.";
+  } else if (windspeed < 74 && windspeed >= 39) {
+    cat = "Tropical Storm.";
+  } else {
+    cat = "The skies are calm...";
+  }
+
+  let op = document.getElementById("hurricane-output");
+  op.innerHTML = cat;
+
   ///////////////////////////////// DO NOT MODIFY
   check('hurricane', windspeed); // DO NOT MODIFY
   ///////////////////////////////// DO NOT MODIFY
